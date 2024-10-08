c65dbcb52df157d24c0b8a6987eb691ffb2bec26
arenn23 <alan@copia.io>
2024-10-07 19:26:11 -0700

    replace ts-node and nodemon with tsx

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import cors from "cors";
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import roomRoutes from "./routers/roomRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/rooms", roomRoutes);
 
-app.use(express.static(path.join(__dirname, "UI/dist")));
+app.use(express.static(path.join(__dirname, "../UI/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "UI/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../UI/dist/index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "UI/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../UI/dist/index.html"));
 });
 
 app.listen(PORT, () => {
